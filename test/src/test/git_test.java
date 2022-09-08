64d0d27ad4c250ed82e978d71fe2ceae7c9280b7
Mekhi Wood-Brock <mekhiwb966@gmail.com>
2022-09-08 19:11:44 -0400

    please work..

--- test/src/test/git_test.java
+++ test/src/test/git_test.java
@@ -4,7 +4,7 @@ public class git_test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("trying to push to github");
+		System.out.println("trying to push it to github");
 
 	}
 
